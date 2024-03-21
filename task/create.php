31a5843ac610c84501053359ee87d314a5bc1f83
Colin0429 <149382086+Colin0429@users.noreply.github.com>
2024-03-21 11:34:45 +0100

    Update create.php
    In tasks/create.php een formulier opzetten.
    
    Nog niet met controller enzo verbonden.

--- task/create.php
+++ task/create.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="nl">
 
 <head>
@@ -6,7 +6,31 @@
 </head>
 
 <body>
-    
+
+    <div class="container">
+        <h1>Formulier</h1>
+
+            <div class="form-group">
+                <label for="naam">Jouw naam:</label>
+                <input type="naam" name="naam" id="naam" class="form-input">
+            </div>
+
+            <div class="form-group">
+                <label for="opdracht">Opdracht naam:</label>
+                <input type="text" name="opdracht" id="opdracht" class="form-input">
+            </div>
+
+            <div class="form-group">
+                <label for="extra">Extra Info?</label>
+                <textarea name="extra" id="extra" class="extra" rows="4"></textarea>
+            </div>
+
+
+            <input type="submit" value="Verstuur melding">
+
+        </form>
+    </div>
+
 </body>
 
 </html>
