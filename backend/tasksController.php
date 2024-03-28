8d2553dc3b51538504ed1eaca8cf0f342b3ef406
Bowieman12 <Larsschipperen@gmail.com>
2024-03-28 14:13:06 +0100

    ticket 1.3
    aanpas knop toegevoegd

--- backend/tasksController.php
+++ backend/tasksController.php
@@ -19,5 +19,46 @@ if($action == 'create'){
     ":titel"             => $titel,
     ":beschrijving"      => $beschrijving,
     ":afdeling"          => $afdeling,
-]);
+    ]);
+
+    $msg = "Je melding is verwijderd.";
+    header("location:../../../task/index.php?msg=$msg");
+
+}
+if($action == 'update'){
+    $titel = $_POST['titel'];
+    $beschrijving = $_POST['beschrijving'];
+    $afdeling = $_POST['afdeling'];
+    $status = $_POST['status']
+    require_once '../config/conn.php';
+
+    $query =    "UPDATE taken 
+                SET titel = :titel, beschrijving = :beschrijving, afdeling = :afdeling, status = :status
+                WHERE id = :id";
+
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":titel"             => $titel,
+        ":beschrijving"      => $beschrijving,
+        ":afdeling"          => $afdeling,
+        ":status"            => $status
+
+    ]);
+    head("location: ../../../task/index.php?msg=$msg");
+
+}
+if($action == 'delete'){
+    $id = $_POST['id'];
+
+    require_once '../config/conn.php';
+    $query = "DELETE FROM taken  
+              WHERE id = :id;";
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":id"        => $id,
+    ]);
+
+    $msg = "Je melding is verwijderd.";
+    head("location: ../../../task/index.php?msg=$msg");
+
 }
