92b9896f86a5fbe5295f7cd39c0540f4db7d6746
Bowieman12 <Larsschipperen@gmail.com>
2024-04-10 10:34:45 +0200

    kleine fixes

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
         <h1> Hoii hoii, kom je je taken bekijken in onze fantastische online takenlijst! </h1>
         <a href ="task/index.php">Hier is je takenlijst</a>
     </div>
+    <div class="container">
+    <button onclick="window.location.href = 'task/login.php';">Go to Login</button>
+    </div>
 
 </body>
 
