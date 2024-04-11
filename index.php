52fac7f5aacf6bea020766f117bd5455edcf02af
KimLazaroms <kimlazaroms@gmail.com>
2024-04-11 12:38:44 +0200

    1.6 Header opmaak

--- index.php
+++ index.php
@@ -11,14 +11,11 @@
         <?php require_once 'header.php'; ?>
     </header>
     
-<<<<<<< HEAD
     <main>
         <div class="container">
             <h1> Hoii hoii, kom je je taken bekijken in onze fantastische online takenlijst! </h1>
-            <a href ="task/index.php">Hier is je takenlijst</a>
         </div>
     </main>
-
     <footer>
 
     </footer>
