c4ed8f9da2c47b33647d71ca16a5b555cf5ca582
KimLazaroms <kimlazaroms@gmail.com>
2024-04-16 13:23:21 +0200

    CSS aanpassing
    Style overgezet naar main.css
    
    Header toegevoegd

--- task/index.php
+++ task/index.php
@@ -4,71 +4,53 @@
 <head>
     <title>Takenlijst</title>
     <?php require_once '../head.php'; ?>
-    <style>
-        .container {
-            width: 500px;
-            height: 650px;
-            border: 2px solid #ccc;
-            margin: 20px;
-            float: left;
-        }
-        .item {
-            width: 450px;
-            height: 50px;
-            background-color: #009688;
-            color: #fff;
-            text-align: center;
-            line-height: 50px;
-            margin: 10px;
-            cursor: grab;
-        }
-        .container-placement {
-            display: flex;
-            justify-content: center;
-        }
-
-    </style>
 </head>
 
 <body>
-    <button onclick="window.location.href = 'logout.php';">Logout</button>
-    <div class="kanban">
-    <h1>Taken</h1>
-        <a href="create.php">Nieuwe taak &gt;</a>
-        <?php if(isset($_GET['msg']))
-        { 
-            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
-        } ?>
+    <header>
+        <?php require_once '../header.php'; ?>
+    </header>
 
-        <?php
-            require_once '../config/conn.php';
-            $query  = "SELECT * FROM taken";
-            $statement = $conn->prepare($query);
-            $statement->execute();
-            $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
-        ?>
+    <main>
+        <button onclick="window.location.href = 'logout.php';">Logout</button>
+        <div class="kanban">
+        <h1>Taken</h1>
+            <a href="create.php">Nieuwe taak &gt;</a>
+            <?php if(isset($_GET['msg']))
+            { 
+                echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+            } ?>
 
-    </div>
+            <?php
+                require_once '../config/conn.php';
+                $query  = "SELECT * FROM taken";
+                $statement = $conn->prepare($query);
+                $statement->execute();
+                $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
+            ?>
 
-    <div class="container-placement">
-        <div class="container" id="container1" ondrop="drop(event)" ondragover="allowDrop(event)">
-            <?php $i = 0; ?> 
-            <?php foreach($taken as $taak): ?>
-                <div class="item" id="item<?php echo $i++; ?>" draggable="true" ondragstart="drag(event)">
-                    <?php echo $taak['afdeling']; ?>
-                    <a href="exempel.php?id=<?php echo $taak['id']; ?>"><?php echo $taak['titel']; ?></a>
-                    <a href="edit.php?id=<?php echo $taak['id']; ?>">aanpassen </a>
-                </div>
-            <?php endforeach; ?>
         </div>
-    
 
-        <div class="container" id="container2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-    
+        <div class="container-placement">
+            <div class="container-index" id="container1" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <?php $i = 0; ?> 
+                <?php foreach($taken as $taak): ?>
+                    <div class="item" id="item<?php echo $i++; ?>" draggable="true" ondragstart="drag(event)">
+                        <?php echo $taak['afdeling']; ?>
+                        <a href="exempel.php?id=<?php echo $taak['id']; ?>"><?php echo $taak['titel']; ?></a>
+                        <a href="edit.php?id=<?php echo $taak['id']; ?>">aanpassen </a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        
 
-        <div class="container" id="container3" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+            <div class="container-index" id="container2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+        
 
-    </div>
+            <div class="container-index" id="container3" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+
+        </div>
+    </main>
 
 <script>
     function allowDrop(event) {
