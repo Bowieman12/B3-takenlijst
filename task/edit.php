ff3aae08fbe6ad78ed2eba7b3bde104acf50bdc8
KimLazaroms <kimlazaroms@gmail.com>
2024-04-16 13:51:04 +0200

    Header invoeging
    Bij edit en signup de header ingevoegd

--- task/edit.php
+++ task/edit.php
@@ -7,80 +7,86 @@
 </head>
 
 <body>
-    <?php 
+    <header>
+        <?php require_once '../header.php'; ?>
+    </header>
 
-    if(!isset($_GET['id'])){
-        echo "Geef in je aanpaslink op de index.php het id van betreffende item mee achter de URL in je a-element om deze pagina werkend te krijgen na invoer van je vijfstappenplan";
-        exit;
+    <main>
+        <?php 
 
-    }
-    ?>
-    <?php
-        require_once '../head.php'; ?>
+        if(!isset($_GET['id'])){
+            echo "Geef in je aanpaslink op de index.php het id van betreffende item mee achter de URL in je a-element om deze pagina werkend te krijgen na invoer van je vijfstappenplan";
+            exit;
 
-    <div class="container">
-        <h1>Taak aanpassen</h1>
-
-        <?php
-        $id = $_GET['id'];
-
-        require_once '../config/conn.php';
-
-        $query = "SELECT * FROM taken WHERE id = :id";
-
-        $statement = $conn->prepare($query);
-
-        $statement->execute([
-            ':id' => $id,
-        ]);
-
-        $taak = $statement->fetch(PDO::FETCH_ASSOC);
+        }
         ?>
+        <?php
+            require_once '../head.php'; ?>
 
-        <form action="<?php echo $base_url; ?>/backend/tasksController.php" method="POST">
-            <input type="hidden" name="action" value="update"> 
-            <input type="hidden" name="id" value="<?php echo $taak['id'];?>">
+        <div class="container">
+            <h1>Taak aanpassen</h1>
+
+            <?php
+            $id = $_GET['id'];
+
+            require_once '../config/conn.php';
+
+            $query = "SELECT * FROM taken WHERE id = :id";
+
+            $statement = $conn->prepare($query);
+
+            $statement->execute([
+                ':id' => $id,
+            ]);
+
+            $taak = $statement->fetch(PDO::FETCH_ASSOC);
+            ?>
+
+            <form action="<?php echo $base_url; ?>/backend/tasksController.php" method="POST">
+                <input type="hidden" name="action" value="update"> 
+                <input type="hidden" name="id" value="<?php echo $taak['id'];?>">
 
 
-            <div class="form-group"> 
-                <label for="titel">Titel:  </label>
-                <input type="text" name="titel" id="titel" class="form-input" value="<?php echo ($taak['titel']);?>">
-            </div>
-            <div class="form-group">
-                <label for="beschrijving">beschrijving: </label>
-                <textarea name="beschrijving" id="beschrijving" class="form-input" rows="4" ><?php echo $taak['beschrijving']?></textarea>   
-            </div>beschrijving
-            <div class="form-group"> 
-                <label for="afdeling">afdeling: </label>
-                <input type="text" name="afdeling" id="afdeling" class="form-input" value="<?php echo ($taak['afdeling']);?>">
-            </div>
-            <div class="form-group"> 
-                <label for="status">status: </label>
-                <select name="status" id="status" class="form-input">
-                    <?php
-                    $status_options = array("To-Do", "Doing", "Done");
-                    foreach ($status_options as $option) {
-                        if ($option == $taak['status']) {
-                            echo "<option value=\"$option\" selected>$option</option>";
-                        } else {
-                            echo "<option value=\"$option\">$option</option>";
+                <div class="form-group"> 
+                    <label for="titel">Titel:  </label>
+                    <input type="text" name="titel" id="titel" class="form-input" value="<?php echo ($taak['titel']);?>">
+                </div>
+                <div class="form-group">
+                    <label for="beschrijving">beschrijving: </label>
+                    <textarea name="beschrijving" id="beschrijving" class="form-input" rows="4" ><?php echo $taak['beschrijving']?></textarea>   
+                </div>beschrijving
+                <div class="form-group"> 
+                    <label for="afdeling">afdeling: </label>
+                    <input type="text" name="afdeling" id="afdeling" class="form-input" value="<?php echo ($taak['afdeling']);?>">
+                </div>
+                <div class="form-group"> 
+                    <label for="status">status: </label>
+                    <select name="status" id="status" class="form-input">
+                        <?php
+                        $status_options = array("To-Do", "Doing", "Done");
+                        foreach ($status_options as $option) {
+                            if ($option == $taak['status']) {
+                                echo "<option value=\"$option\" selected>$option</option>";
+                            } else {
+                                echo "<option value=\"$option\">$option</option>";
+                            }
                         }
-                    }
-                    ?>
-                </select>           
-            </div>
-            <input type="submit" value="taak opslaan">
+                        ?>
+                    </select>           
+                </div>
+                <input type="submit" value="taak opslaan">
 
-        </form>
+            </form>
 
-        <hr>
+            <hr>
 
-        <form action="<?php echo $base_url; ?>/backend/tasksController.php" method="POST">
-            <input type="hidden" name="action"value="delete">
-            <input type="hidden" name="id"value="<?php echo $id;?>">
-            <input type="submit" value="Verwijderen">
-        </form>
-    </div>  
+            <form action="<?php echo $base_url; ?>/backend/tasksController.php" method="POST">
+                <input type="hidden" name="action"value="delete">
+                <input type="hidden" name="id"value="<?php echo $id;?>">
+                <input type="submit" value="Verwijderen">
+            </form>
+        </div>
+    </main>  
 
 </body>
 
--- task/signup.php
+++ task/signup.php
@@ -7,21 +7,27 @@
 </head>
 
 <body>
-    <h2>Sign Up</h2>
-    <form action="../backend/tasksController.php" method="POST">
-        <input type="hidden" name="action" value="signup">
+    <header>
+        <?php require_once '../header.php'; ?>
+    </header>
+    
+    <main>
+        <h2>Sign Up</h2>
+        <form action="../backend/tasksController.php" method="POST">
+            <input type="hidden" name="action" value="signup">
 
-        <label for="naam">Naam: </label>
-        <input type="text" id="naam" name="naam" required>
-        <br><br>
-        <label for="username">Username: </label>
-        <input type="text" id="username" name="username" required>
-        <br><br>
-        <label for="password">Password: </label>
-        <input type="password" id="password" name="password" required>
-        <br><br>
-        <input type="submit" value="Sign Up">
-    </form>
+            <label for="naam">Naam: </label>
+            <input type="text" id="naam" name="naam" required>
+            <br><br>
+            <label for="username">Username: </label>
+            <input type="text" id="username" name="username" required>
+            <br><br>
+            <label for="password">Password: </label>
+            <input type="password" id="password" name="password" required>
+            <br><br>
+            <input type="submit" value="Sign Up">
+        </form>
+    </main>
 </body>
 
 </html>
