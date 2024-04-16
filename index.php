fb42dda84858e469ab2cc0f2a66a0e8bbe75ce4d
KimLazaroms <kimlazaroms@gmail.com>
2024-04-16 13:23:42 +0200

    BLANK

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <header>
         <?php require_once 'header.php'; ?>
     </header>
-    
     <main>
         <div class="container">
             <h1> Hoii hoii, kom je je taken bekijken in onze fantastische online takenlijst! </h1>
