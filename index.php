e8a5659d38bb0f5c3b144121c26cc554956dc256
Colin0429 <149382086+Colin0429@users.noreply.github.com>
2024-04-17 10:32:43 +0200

    Homepage gemaakt
    Iederen deelnemer heeft een blok.
    Informatie over taken van deze persoon in het informatie blok.

--- index.php
+++ index.php
@@ -14,6 +14,39 @@
         <div class="container">
             <h1> Hoii hoii, kom je je taken bekijken in onze fantastische online takenlijst! </h1>
         </div>
+
+        <div class="blok">
+            <div class="blok-persoon">
+                <h3>Colin</h3>
+                <img src="https://i.imgur.com/WPC0JQS.png" alt="foto">
+                <p>Colin heeft aan verschillende taken gewerkt. Waaronder het maken van de homepage, 
+                    Een zichtbaar overzicht / lijst gemaakt van alle taken waarvan de status niet "done" is en een paar taken zoals een formulier opzetten, en de controller volledig opgezet.</p>
+            </div>
+
+            <div class="blok-persoon">
+                <h3>Daan</h3>
+                <img src="https://i.imgur.com/p939sRZ.png" alt="foto">
+                <p>Daan heeft aan verschillende taken gewerkt. Waaronder niewe taken aangemaakt, zoals de index linken met de create pagina, verschillende inputs getest, een aanpas knop en een verwijder knop gemaakt bij de taken.
+                    Ook heeft Daan aan het gebruiker overzicht gewerkt samen met Lars. </p>
+            </div>
+
+            <div class="blok-persoon">
+                <h3>Kim</h3>
+                <img src="https://i.imgur.com/BsdW9lx.png" alt="foto">
+                <p>Kim heeft aan verschillende taken gewerkt. Waaronder het geven van taken aan iedereen in het team en een acceptatietest gemaakt, dit gaat over de header, takenlijst, invoegen van taken en inloggen.</p>
+            </div>
+
+            <div class="blok-persoon">
+                <h3>Lars</h3>
+                <img src="https://i.imgur.com/N9uyppU.png" alt="foto">
+                <p>Lars heeft aan verschillende taken gewerkt. Waaronder een wireframe gemaakt voor de website, met Daan aan een aanpas en verwijder knop gewerkt voor de taken-pagina. 
+                    Ook heeft Lars aan het gebruiker overzicht gewerkt samen met Daan.
+                </p>
+            </div>
+
+            
+        </div>
+
     </main>
     <footer>
 
